edacb70777a9b9dd4ac3608f8aa5cfe5fe4e7eb6
Fatih Balsoy <28816254+fatihbalsoy@users.noreply.github.com>
2023-02-28 13:31:20 -0500

    Initial Commit

---
+++ EscapeKey/EscapeKeyApp.swift
@@ -0,0 +1,17 @@
+//
+//  EscapeKeyApp.swift
+//  EscapeKey
+//
+//  Created by Fatih Balsoy on 2/28/23.
+//
+
+import SwiftUI
+
+@main
+struct EscapeKeyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
